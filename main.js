b9fba648661d1e3e3da437fba47f253bbbde3fd4
Bakugan1024 <85323092+Bakugan1024@users.noreply.github.com>
2022-04-29 18:22:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,6 +20,11 @@ function setup() {
 
 function draw() {
     background('#8B0000');
+
+    document.getElementById("square_side").innerHTML="Width and Height of a Square will be="+difference+"px";
+    fill('#F90093');
+    stroke('F90093');
+    square(noseX,noseY,difference);
 }
 
 
@@ -25,5 +36,14 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+            noseX=results[0].pose.pose.x;
+                noseY=results[0].pose.pose.y;
+                    console.log("noseX="+noseX+"noseY"+noseY);
+        
+        leftWristX=results[0].pose.leftWrist.x;
+            rightWristX=results[0].pose.rightWrist.x;
+                difference=floor(leftWristX-rightWristX);
+
+                    console.log("leftWristX="+leftWristX+"rightWristX"+rightWristX+"difference"+difference);
     }
 }
